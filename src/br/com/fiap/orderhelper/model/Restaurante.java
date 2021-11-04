c9e8c0f2fc9a24198a636cc677effb8f548cf4d3
Jorge Zarlenga Regiani <jorge.zarlenga@outlook.com>
2021-11-04 20:40:46 -0300

    Fixed Restaurante Class

--- src/br/com/fiap/orderhelper/model/Restaurante.java
+++ src/br/com/fiap/orderhelper/model/Restaurante.java
@@ -1,212 +1,152 @@
 package br.com.fiap.orderhelper.model;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+
+/**
+ * Classe que abstrai o Restaurante
+ * 
+ * @author Dionney Saraiva, Jorge Zarlenga, Maria Eduarda e Rodrigo Valori
+ * @version 1.0
+ */
+
 public class Restaurante implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 
 	// Atributos:
 
-		private int codRestaurante;
-		private String nomeRestaurante;
-		private int numeroCep;
-		private int numeroLogradouro;
-		private GregorianCalendar horaInicioFuncionamento;
-		private GregorianCalendar horaTerminoFuncionamento;
-		private String categoriaRestaurante;
-		private int avaliacao;
-		private int quantidadeCozinheiros;
-		private int quantidadeEntregadores;
-		private double raioAtuacaoKm;
+	private int codRestaurante;
+	private String nomeRestaurante;
+	private int numeroCep;
+	private int numeroLogradouro;
+	private GregorianCalendar horaInicioFuncionamento;
+	private GregorianCalendar horaTerminoFuncionamento;
+	private String categoriaRestaurante;
+	private int avaliacao;
+	private int quantidadeCozinheiros;
+	private int quantidadeEntregadores;
+	private double raioAtuacaoKm;
 
-		// Construtor vazio:
+	// Construtor vazio:
 
-		public Restaurante() {
+	public Restaurante() {
 
-		}
+	}
 		
-		// Construtor que recebe todos parametros:
+	// Construtor que recebe todos parametros:
+	public Restaurante(int codRestaurante, String nomeRestaurante, int numeroCep, int numeroLogradouro,
+			GregorianCalendar horaInicioFuncionamento, GregorianCalendar horaTerminoFuncionamento,
+			String categoriaRestaurante, int avaliacao, int quantidadeCozinheiros, int quantidadeEntregadores,
+			double raioAtuacaoKm) {
+		super();
+		this.codRestaurante = codRestaurante;
+		this.nomeRestaurante = nomeRestaurante;
+		this.numeroCep = numeroCep;
+		this.numeroLogradouro = numeroLogradouro;
+		this.horaInicioFuncionamento = horaInicioFuncionamento;
+		this.horaTerminoFuncionamento = horaTerminoFuncionamento;
+		this.categoriaRestaurante = categoriaRestaurante;
+		this.avaliacao = avaliacao;
+		this.quantidadeCozinheiros = quantidadeCozinheiros;
+		this.quantidadeEntregadores = quantidadeEntregadores;
+		this.raioAtuacaoKm = raioAtuacaoKm;
+	}
+	
 
-		public Restaurante(int codAlimento, GregorianCalendar dataHoraAlimento, String nomeAlimento, double energiaKcalAlimento, String descricaoAlimento) {
-			this.codAlimento = codAlimento;
-			this.dataHoraAlimento = dataHoraAlimento;
-			this.nomeAlimento = nomeAlimento;
-			this.energiaKcalAlimento = energiaKcalAlimento;
-			this.descricaoAlimento = descricaoAlimento;
-		}
-
-		// Metodos getters and setters:
-		
-		public int getCodRestaurante() {
-			return codRestaurante;
-		}
-
-		public void setCodRestaurante(int codRestaurante) {
-			this.codRestaurante = codRestaurante;
-		}
-
-		public String getNomeRestaurante() {
-			return nomeRestaurante;
-		}
-
-		public void setNomeRestaurante(String nomeRestaurante) {
-			this.nomeRestaurante = nomeRestaurante;
-		}
-
-		public int getNumeroCep() {
-			return numeroCep;
-		}
-
-		public void setNumeroCep(int numeroCep) {
-			this.numeroCep = numeroCep;
-		}
-
-		public int getNumeroLogradouro() {
-			return numeroLogradouro;
-		}
-
-		public void setNumeroLogradouro(int numeroLogradouro) {
-			this.numeroLogradouro = numeroLogradouro;
-		}
-
-		public GregorianCalendar getHoraInicioFuncionamento() {
-			return horaInicioFuncionamento;
-		}
-
-		public void setHoraInicioFuncionamento(GregorianCalendar horaInicioFuncionamento) {
-			this.horaInicioFuncionamento = horaInicioFuncionamento;
-		}
-
-		public GregorianCalendar getHoraTerminoFuncionamento() {
-			return horaTerminoFuncionamento;
-		}
-
-		public void setHoraTerminoFuncionamento(GregorianCalendar horaTerminoFuncionamento) {
-			this.horaTerminoFuncionamento = horaTerminoFuncionamento;
-		}
-
-		public String getCategoriaRestaurante() {
-			return categoriaRestaurante;
-		}
-
-		public void setCategoriaRestaurante(String categoriaRestaurante) {
-			this.categoriaRestaurante = categoriaRestaurante;
-		}
-
-		public int getAvaliacao() {
-			return avaliacao;
-		}
-
-		public void setAvaliacao(int avaliacao) {
-			this.avaliacao = avaliacao;
-		}
-
-		public int getQuantidadeCozinheiros() {
-			return quantidadeCozinheiros;
-		}
-
-		public void setQuantidadeCozinheiros(int quantidadeCozinheiros) {
-			this.quantidadeCozinheiros = quantidadeCozinheiros;
-		}
-
-		public int getQuantidadeEntregadores() {
-			return quantidadeEntregadores;
-		}
-
-		public void setQuantidadeEntregadores(int quantidadeEntregadores) {
-			this.quantidadeEntregadores = quantidadeEntregadores;
-		}
-
-		public double getRaioAtuacaoKm() {
-			return raioAtuacaoKm;
-		}
-
-		public void setRaioAtuacaoKm(double raioAtuacaoKm) {
-			this.raioAtuacaoKm = raioAtuacaoKm;
-		}
-		
-		
-}
-
-
-
-
-
-
-
-
-package br.com.fiap.healthtrack.model;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.GregorianCalendar;
-import java.util.List;
-
-/**
- * Classe que abstrai o atributo alimento
- * 
- * @author Dionney Saraiva, Jorge Zarlenga, Maria Eduarda e Rodrigo Valori
- * @version 1.0
- */
-
-public class Alimento implements Serializable {
-
-	private static final long serialVersionUID = 1L;
+	// Metodos getters and setters:
+	
+	public int getCodRestaurante() {
+		return codRestaurante;
+	}
 
 	
 
-	// Construtor que recebe todos parametros:
-
-	public Alimento(int codAlimento, GregorianCalendar dataHoraAlimento, String nomeAlimento, double energiaKcalAlimento, String descricaoAlimento) {
-		this.codAlimento = codAlimento;
-		this.dataHoraAlimento = dataHoraAlimento;
-		this.nomeAlimento = nomeAlimento;
-		this.energiaKcalAlimento = energiaKcalAlimento;
-		this.descricaoAlimento = descricaoAlimento;
+	public void setCodRestaurante(int codRestaurante) {
+		this.codRestaurante = codRestaurante;
 	}
 
-	// Metodos getters and setters:
-
-	public int getCodAlimento() {
-		return codAlimento;
+	public String getNomeRestaurante() {
+		return nomeRestaurante;
 	}
 
-	public void setCodAlimento(int codAlimento) {
-		this.codAlimento = codAlimento;
+	public void setNomeRestaurante(String nomeRestaurante) {
+		this.nomeRestaurante = nomeRestaurante;
 	}
 
-	public GregorianCalendar getDataHoraAlimento() {
-		return dataHoraAlimento;
+	public int getNumeroCep() {
+		return numeroCep;
 	}
 
-	public void setDataHoraAlimento(GregorianCalendar dataHoraAlimento) {
-		this.dataHoraAlimento = dataHoraAlimento;
+	public void setNumeroCep(int numeroCep) {
+		this.numeroCep = numeroCep;
 	}
 
-	public String getNomeAlimento() {
-		return nomeAlimento;
+	public int getNumeroLogradouro() {
+		return numeroLogradouro;
 	}
 
-	public void setNomeAlimento(String nomeAlimento) {
-		this.nomeAlimento = nomeAlimento;
+	public void setNumeroLogradouro(int numeroLogradouro) {
+		this.numeroLogradouro = numeroLogradouro;
 	}
 
-	public double getEnergiaKcalAlimento() {
-		return energiaKcalAlimento;
+	public GregorianCalendar getHoraInicioFuncionamento() {
+		return horaInicioFuncionamento;
 	}
 
-	public void setEnergiaKcalAlimento(double gastoKcalAlimento) {
-		this.energiaKcalAlimento = gastoKcalAlimento;
+	public void setHoraInicioFuncionamento(GregorianCalendar horaInicioFuncionamento) {
+		this.horaInicioFuncionamento = horaInicioFuncionamento;
 	}
 
-	public String getDescricaoAlimento() {
-		return descricaoAlimento;
+	public GregorianCalendar getHoraTerminoFuncionamento() {
+		return horaTerminoFuncionamento;
 	}
 
-	public void setDescricaoAlimento(String descricaoAlimento) {
-		this.descricaoAlimento = descricaoAlimento;
+	public void setHoraTerminoFuncionamento(GregorianCalendar horaTerminoFuncionamento) {
+		this.horaTerminoFuncionamento = horaTerminoFuncionamento;
 	}
 
+	public String getCategoriaRestaurante() {
+		return categoriaRestaurante;
+	}
+
+	public void setCategoriaRestaurante(String categoriaRestaurante) {
+		this.categoriaRestaurante = categoriaRestaurante;
+	}
+
+	public int getAvaliacao() {
+		return avaliacao;
+	}
+
+	public void setAvaliacao(int avaliacao) {
+		this.avaliacao = avaliacao;
+	}
+
+	public int getQuantidadeCozinheiros() {
+		return quantidadeCozinheiros;
+	}
+
+	public void setQuantidadeCozinheiros(int quantidadeCozinheiros) {
+		this.quantidadeCozinheiros = quantidadeCozinheiros;
+	}
+
+	public int getQuantidadeEntregadores() {
+		return quantidadeEntregadores;
+	}
+
+	public void setQuantidadeEntregadores(int quantidadeEntregadores) {
+		this.quantidadeEntregadores = quantidadeEntregadores;
+	}
+
+	public double getRaioAtuacaoKm() {
+		return raioAtuacaoKm;
+	}
+
+	public void setRaioAtuacaoKm(double raioAtuacaoKm) {
+		this.raioAtuacaoKm = raioAtuacaoKm;
+	}
+	
 	// Metodos:
 
 	/**
@@ -268,11 +208,13 @@ public class Alimento implements Serializable {
 
 		return exclusaoAlimentoFinalizada;
 	}
-
+			
+	// Método toString:
 	@Override
-	public String toString() {
-		return "Alimento [codAlimento=" + codAlimento + ", dataHoraAlimento=" + dataHoraAlimento + ", nomeAlimento="
-				+ nomeAlimento + ", gastoKcalAlimento=" + gastoKcalAlimento + ", descricaoAlimento="
-				+ descricaoAlimento + "]";
-	}
+		public String toString() {
+			return "Restaurante [codRestaurante=" + codRestaurante + ", nomeRestaurante=" + nomeRestaurante + ", numeroCep="
+					+ numeroCep + ", numeroLogradouro=" + numeroLogradouro + ", categoriaRestaurante="
+					+ categoriaRestaurante + ", avaliacao=" + avaliacao + ", quantidadeCozinheiros=" + quantidadeCozinheiros
+					+ ", quantidadeEntregadores=" + quantidadeEntregadores + ", raioAtuacaoKm=" + raioAtuacaoKm + "]";
+		}
 }
